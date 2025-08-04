388b3bb0ae0dd7981a63706a0b4041e23e5e4f74
Devinda Wijesinghe <148736108+devindazz@users.noreply.github.com>
2025-08-04 22:44:56 +0530

    feat: implement user registration and authentication

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,10 +2,31 @@ import asyncHandler from "express-async-handler";
 import User from "../models/userModel.js";
 
 const registerUser = asyncHandler (async (req, res) => {
-    console.log(req.body);
+    const { name, email, password } = req.body;
     
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+        res.status(400);
+        throw new Error('User already exists');
+    }
 
-    res.status(200).json({ message: 'Register user' })
+    const user = await User.create({
+        name,
+        email,
+        password
+    });
+
+    if (user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+        });
+    } else {
+        res.status(400);
+        throw new Error('Invalid user data');
+    }
+
 });
 
 const authUser = asyncHandler (async (req, res) => {
