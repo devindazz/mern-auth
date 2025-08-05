d257a42fd9a00247e87803a77b258acab4d6c891
Devinda Wijesinghe <148736108+devindazz@users.noreply.github.com>
2025-08-06 01:38:01 +0530

    feat: add profile screen

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -17,6 +17,7 @@ const router = createBrowserRouter(
       <Route index={true} path='/' element={<HomeScreen />} />
       <Route path='/login' element={<LoginScreen />} />
       <Route path='/register' element={<RegisterScreen />} />
+      <Route path='/profile' element={<ProfileScreen />} />
     </Route>  
   )
 );
