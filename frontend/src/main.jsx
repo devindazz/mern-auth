c4c2868d24952496f5113b5a12ecc133e7a1271e
Devinda Wijesinghe <148736108+devindazz@users.noreply.github.com>
2025-08-06 00:12:59 +0530

    feat: add basic styling and setup for frontend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,6 +7,8 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import HomeScreen from './screens/homeScreen.jsx';
 import LoginScreen from './screens/LoginScreen.jsx';
 import RegisterScreen from './screens/RegisterScreen.jsx';
+import store from './store.js';
+import {Provider} from 'react-redux';
 
 
 const router = createBrowserRouter(
@@ -19,7 +21,9 @@ const router = createBrowserRouter(
   )
 );
 ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>,
+  </Provider>
 );
