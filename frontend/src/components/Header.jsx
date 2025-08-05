e1912d88f73acf443bf3c77807ae68c8e73cb0c7
Devinda Wijesinghe <148736108+devindazz@users.noreply.github.com>
2025-08-05 15:49:42 +0530

    feat: add react-router-bootstrap and update package.json and package-lock.json

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,17 +1,24 @@
 import {Navbar, Nav, Container} from 'react-bootstrap';
 import {FaSignInAlt, FaSignOutAlt} from 'react-icons/fa';
+import {LinkContainer} from 'react-router-bootstrap';
 
 const Header = () => {
     return (
         <header>
             <Navbar bg="dark" variant="dark" expand="lg">
                 <Container>
-                    <Navbar.Brand href="/">MERN Auth</Navbar.Brand>
+                    <LinkContainer to="/">
+                    <Navbar.Brand>MERN Auth</Navbar.Brand>
+                    </LinkContainer>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="me-auto">
-                            <Nav.Link href="/login"><FaSignInAlt /> Login</Nav.Link>
-                            <Nav.Link href="/register"><FaSignInAlt /> Register</Nav.Link>
+                            <LinkContainer to="/login">
+                            <Nav.Link><FaSignInAlt /> Login</Nav.Link>
+                            </LinkContainer>
+                            <LinkContainer to="/register">
+                            <Nav.Link><FaSignInAlt /> Register</Nav.Link>
+                            </LinkContainer>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
