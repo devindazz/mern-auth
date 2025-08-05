1aed08a32cbdbec8b44bbd1f9d1c4885bdbdf44e
Devinda Wijesinghe <148736108+devindazz@users.noreply.github.com>
2025-08-05 13:32:35 +0530

    feat: implement user authentication and profile management

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,12 +1,11 @@
 import express from 'express';
 const router = express.Router();
 import { authUser,registerUser,logoutUser,getUserProfile,updateUserProfile } from '../controllers/userController.js';
-
+import { protect } from '../middleware/authMiddleware.js';
 
 router.post('/',registerUser);
 router.post('/auth',authUser);
 router.post('/logout',logoutUser);
-router.get('/profile',getUserProfile);
-router.put('/profile',updateUserProfile);
+router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile);
 
 export default router;
