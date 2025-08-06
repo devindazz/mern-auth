0f4f8ce36503d1bb0ad8f94662d3efba1ca29ce3
Devinda Wijesinghe <148736108+devindazz@users.noreply.github.com>
2025-08-06 12:37:48 +0530

    feat: add profile screen and private route

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -9,6 +9,8 @@ import LoginScreen from './screens/LoginScreen.jsx';
 import RegisterScreen from './screens/RegisterScreen.jsx';
 import store from './store.js';
 import {Provider} from 'react-redux';
+import ProfileScreen from './screens/profileScreen.jsx';
+import PrivateRoute from './components/PrivateRoute.jsx';
 
 
 const router = createBrowserRouter(
@@ -17,7 +19,9 @@ const router = createBrowserRouter(
       <Route index={true} path='/' element={<HomeScreen />} />
       <Route path='/login' element={<LoginScreen />} />
       <Route path='/register' element={<RegisterScreen />} />
-      <Route path='/profile' element={<ProfileScreen />} />
+      <Route element={<PrivateRoute />}>
+        <Route path='/profile' element={<ProfileScreen />} />
+      </Route>
     </Route>  
   )
 );
--- frontend/src/screens/profileScreen.jsx
+++ frontend/src/screens/profileScreen.jsx
@@ -1,1 +1,102 @@
+import { useState, useEffect } from "react";
+import {Link, useNavigate} from "react-router-dom";
+import { Form, Button } from "react-bootstrap";
+import FormContainer from "../components/FormContainer";
+import { useDispatch, useSelector } from "react-redux";
+import {toast} from "react-toastify";
+import { useRegisterMutation } from "../slices/usersApiSlice";
+import { setCredentials } from "../slices/authSlice";
+import Loader from "../components/loader";
+import { set } from "mongoose";
+
 const ProfileScreen = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const {userInfo} = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    setName(userInfo.name);
+    setEmail(userInfo.email);
+  }, [userInfo.setName,userInfo.setEmail]);
+
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    if (password !== confirmPassword) {
+      toast.error("Passwords do not match");
+      return;
+    } else {
+      console.log("Profile updated with: ", { name, email, password });
+      
+    }
+  };
+
+  return (
+    <FormContainer>
+      <h1>Update Profile</h1>
+      <Form onSubmit={submitHandler}>
+
+        <Form.Group controlId="name">
+          <Form.Label>Name</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </Form.Group>
+
+        <Form.Group controlId="email">
+          <Form.Label>Email Address</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </Form.Group>
+
+        <Form.Group controlId="password">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Enter password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Form.Group>
+
+         <Form.Group controlId="confirmpassword">
+          <Form.Label>Confirm Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Confirm password"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+        </Form.Group>
+
+        
+        {isLoading && <Loader />}
+
+        <Button type="submit" variant="primary" className="mt-3">
+          Sign up
+        </Button>
+
+        <Row className="py-3">
+            <Col>
+                Already have an account ? <Link to="/login">Login</Link>
+            </Col>
+        </Row>
+      </Form>
+    </FormContainer>
+  );
+}
+
+export default ProfileScreen;
